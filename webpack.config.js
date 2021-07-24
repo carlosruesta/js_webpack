ef4b6deac69ed28cf98ef0fbe6ca75a17361d7ee
Carlos Ruesta <carlos@dcide.com.br>
2021-07-24 18:29:57 -0500

    Aula 02 - Config para compilar arquivos HTML, evitando cache de arquivo JS e limpeza de arquivos excluidos (clean)

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-const path = require('path');
+const Path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: "./app/src/js/app.js",
 	output: {
 		filename: "bundle.js",
-		path: path.resolve(__dirname, 'app/dist')
-	}
+		path: Path.resolve(__dirname, 'app/dist'),
+		clean: true,
+
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './app/src/app.html',
+			filename: "app.html",
+			hash: true
+		})
+	]
 }
